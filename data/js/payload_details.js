aeb6de3001bbaaf4a5af32d37eb623dbfc71164e
John Stewart <stewart.ja@gmail.com>
2016-10-01 20:19:03 -0500

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 4388,
-	"created_at": "Sat Oct 01 01:19:02 +0000 2016",
+	"tweets": 4389,
+	"created_at": "Sun Oct 02 01:19:02 +0000 2016",
 	"lang": "en"
 }
---
+++ data/js/tweets/2016_10.js
@@ -0,0 +1,208 @@
+Grailbird.data.tweets_2016_10 = 
+[
+	{
+		"created_at": "Sat Oct 01 01:33:20 +0000 2016",
+		"id": 782030605827600400,
+		"id_str": "782030605827600384",
+		"text": "RT @SophieAScruggs: Leia doesn't have the \"look\" of a general. #altrightstarwars",
+		"truncated": false,
+		"entities": {
+			"hashtags": [
+				{
+					"text": "altrightstarwars",
+					"indices": [
+						63,
+						80
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "SophieAScruggs",
+					"name": "Sophie Scruggs",
+					"id": 2292336725,
+					"id_str": "2292336725",
+					"indices": [
+						3,
+						18
+					]
+				}
+			],
+			"urls": []
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 73977188,
+			"id_str": "73977188",
+			"name": "John Stewart",
+			"screen_name": "jstew511",
+			"location": "Norman, OK",
+			"description": "Father, sports fanatic, historian of chemistry, Center for Teaching Excellence at OU. Interested in #edtech, #gamification, #OER, #GBL, & learning! #goblinFLC",
+			"url": "http://t.co/iCl6AAUPBN",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "http://t.co/iCl6AAUPBN",
+							"expanded_url": "http://johnastewart.org",
+							"display_url": "johnastewart.org",
+							"indices": [
+								0,
+								22
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 553,
+			"friends_count": 637,
+			"listed_count": 96,
+			"created_at": "Sun Sep 13 21:17:20 +0000 2009",
+			"favourites_count": 1155,
+			"utc_offset": -18000,
+			"time_zone": "Central Time (US & Canada)",
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 4386,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C6E2EE",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme2/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme2/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/530569043489468416/LQfviQ7s_normal.jpeg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/530569043489468416/LQfviQ7s_normal.jpeg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/73977188/1400003593",
+			"profile_link_color": "1F98C7",
+			"profile_sidebar_border_color": "C6E2EE",
+			"profile_sidebar_fill_color": "DAECF4",
+			"profile_text_color": "663B12",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"retweeted_status": {
+			"created_at": "Sat Oct 01 01:30:07 +0000 2016",
+			"id": 782029794389164000,
+			"id_str": "782029794389164032",
+			"text": "Leia doesn't have the \"look\" of a general. #altrightstarwars",
+			"truncated": false,
+			"entities": {
+				"hashtags": [
+					{
+						"text": "altrightstarwars",
+						"indices": [
+							43,
+							60
+						]
+					}
+				],
+				"symbols": [],
+				"user_mentions": [],
+				"urls": []
+			},
+			"source": "<a href=\"http://twitter.com/#!/download/ipad\" rel=\"nofollow\">Twitter for iPad</a>",
+			"in_reply_to_status_id": null,
+			"in_reply_to_status_id_str": null,
+			"in_reply_to_user_id": null,
+			"in_reply_to_user_id_str": null,
+			"in_reply_to_screen_name": null,
+			"user": {
+				"id": 2292336725,
+				"id_str": "2292336725",
+				"name": "Sophie Scruggs",
+				"screen_name": "SophieAScruggs",
+				"location": "Charlotte, North Carolina, USA",
+				"description": "artist, animator, soon to be filmmaker. I want to be in the room where it happens. ✨Differently Abled Advocate/Feminist✨",
+				"url": "https://t.co/3NO3xF1ItZ",
+				"entities": {
+					"url": {
+						"urls": [
+							{
+								"url": "https://t.co/3NO3xF1ItZ",
+								"expanded_url": "http://facebook.com/sophilliaplush",
+								"display_url": "facebook.com/sophilliaplush",
+								"indices": [
+									0,
+									23
+								]
+							}
+						]
+					},
+					"description": {
+						"urls": []
+					}
+				},
+				"protected": false,
+				"followers_count": 614,
+				"friends_count": 620,
+				"listed_count": 46,
+				"created_at": "Sun Jan 19 20:45:28 +0000 2014",
+				"favourites_count": 8224,
+				"utc_offset": -14400,
+				"time_zone": "Eastern Time (US & Canada)",
+				"geo_enabled": false,
+				"verified": false,
+				"statuses_count": 17321,
+				"lang": "en",
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": false,
+				"profile_background_color": "0C6A9C",
+				"profile_background_image_url": "http://pbs.twimg.com/profile_background_images/499299576377921538/AD9YQ_pR.png",
+				"profile_background_image_url_https": "https://pbs.twimg.com/profile_background_images/499299576377921538/AD9YQ_pR.png",
+				"profile_background_tile": false,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/768001510575833088/oWhiUzxl_normal.jpg",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/768001510575833088/oWhiUzxl_normal.jpg",
+				"profile_banner_url": "https://pbs.twimg.com/profile_banners/2292336725/1475281752",
+				"profile_link_color": "1B95E0",
+				"profile_sidebar_border_color": "000000",
+				"profile_sidebar_fill_color": "000000",
+				"profile_text_color": "000000",
+				"profile_use_background_image": true,
+				"has_extended_profile": true,
+				"default_profile": false,
+				"default_profile_image": false,
+				"following": false,
+				"follow_request_sent": false,
+				"notifications": false
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": null,
+			"contributors": null,
+			"is_quote_status": false,
+			"retweet_count": 1,
+			"favorite_count": 7,
+			"favorited": false,
+			"retweeted": true,
+			"lang": "en"
+		},
+		"is_quote_status": false,
+		"retweet_count": 1,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": true,
+		"lang": "en"
+	}
+]
